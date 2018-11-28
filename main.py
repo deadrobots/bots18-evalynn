47600eb8a4e5d025457b67b75f102c426fe60c54
Evalynn <evalynn.hepburn@yahoo.com>
2018-11-27 20:40:36 -0500

    I updated bumpy code to work on maze

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ from actions import *
 
 
 def main():
-    driveUntimed(100, 100)
-    ay = 0
-    while ay < 250:
-        ay = abs(accel_y())
-        print(ay)
-        msleep(50)
-    print('I\'m done. Goodnight. Zzz')
-    ao()
+    while True:
+        ay = 0
+        driveUntimed(100, 100)
+        while ay < 250:
+            ay = abs(accel_y())
+            print(ay)
+            msleep(50)
+        drive(-50, -50, 1000)
+        drive(0, 100, 1500)
+        ao()
+        msleep(1000)
 
 
 if __name__ == "__main__":
